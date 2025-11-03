3d7e10b4c5d8973ee22c52732fa1756bc821c58b
prem sagar <1nc23cs034@ncetmail.com>
2025-11-03 15:03:17 +0530

    Add files via upload

---
+++ parking charges.c
@@ -0,0 +1,20 @@
+//Program Description
+//Take two numbers num1 and num2 as input from stdin and you need to find modulus of them.
+//Input Format
+//Two different lines of input contains two integers num1 and num2
+//Output Format
+//You need to print the modulus of num1 and num2
+//Constraints
+//1<=num1,num2<=10000
+//Input-1
+//102
+//60
+//Output-1
+//42
+#include<stdio.h>
+int main(){
+    int a,b;
+scanf("%d %d",&a,&b);
+printf("%d",a%b);
+return 0;
+}
