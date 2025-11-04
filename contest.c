d0f502f2ababf1803988bc02de19cf5c39df7a75
prem sagar <1nc23cs034@ncetmail.com>
2025-11-04 15:29:18 +0530

    Add files via upload

---
+++ contest.c
@@ -0,0 +1,37 @@
+//Program Description
+//In a coding contest, there are two types of problems:
+//Easy problems, which are worth 1 point each
+//Hard problems, which are worth 2 points each
+//To qualify for the next round, a contestant must score at least X points. Professor solved A Easy problems and B Hard problems. Will Professor qualify or not?
+//Input Format
+//A single line input containing three space-separated integers. as follows :
+//X denotes qualify points.
+//A denotes number of easy problems solved by the Professor.
+//B denotes number of hard problems solved by the Professor.
+//Output Format
+//Print the output whether Professor Qualified or not .
+//Constraints
+//0<=X,A,B<=10000
+//Input-1
+//5 3 0
+//Output-1
+//Not Qualify
+//Input-2
+//15 9 3
+//Output-2
+//Qualify
+//----------------------------------------------------------------------------------
+#include <stdio.h>
+
+int main() {
+    int x, a, b;
+    scanf("%d %d %d", &x, &a, &b);
+
+    if (a + 2 * b >= x) {
+        printf("Qualify");
+    } else {
+        printf("Not Qualify");
+    }
+
+    return 0;
+}
