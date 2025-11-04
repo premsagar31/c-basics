e00ff033887c6560d80f42cf0028246659a4f4eb
prem sagar <1nc23cs034@ncetmail.com>
2025-11-04 14:42:52 +0530

    Add files via upload

---
+++ area and perimeter of a circle.c
@@ -0,0 +1,25 @@
+//Program Description
+//Given an integer radius of the circle. Find the area and perimeter of circle.
+//Note: 1. Adjust the output to 2 decimal places after point.
+//          2. Take the value of 'pi' as 3.14
+//Input Format
+//A single line contains an integer Radius
+//Output Format
+//Print the Area and Perimeter of circle with 2 decimal places after the point
+//Constraints
+//1<=Radius<=1000
+//Input-1
+//5
+//Output-1
+//78.50
+//31.40
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int r;
+scanf("%d",&r);
+double area 3.14* r* r;
+double p2 * 3.14 * r;
+printf("%.21f\n%.21f", area,p);
+}
